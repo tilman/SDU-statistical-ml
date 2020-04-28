aa290ab2fb123fb34f559b3c5f98ab1547763870
Piofret <baptiste.piofret@free.fr>
2020-04-28 21:01:05 +0200

    update 4.2

--- 4/4.2.R
+++ 4/4.2.R
@@ -20,33 +20,42 @@ pca_res <- prcomp(train_data[,-1])
 summary(pca_res)
 #95% is 36 PC
 
-pca_95 <- prcomp(train_data[,-1], rank. = 36)
+pca_95 <- prcomp(train_data[,-1], rank. = 25)
 #summary(pca_95)
 train_data_pca <- pca_95$x
 
-datatree <- rpart(V1 ~ ., data=train_data, method="class")
-print(datatree, digits = 3)
+#datatree <- rpart(V1 ~ ., data=train_data, method="class")
+#print(datatree, digits = 3)
 
-predictions1 <- predict(datatree,test_data[,-1], type = "class")
-table(predictions1, test_data$V1)
+#predictions1 <- predict(datatree,test_data[,-1], type = "class")
+#table(predictions1, test_data$V1)
 
 
 
-rf <- randomForest(train_data$V1 ~ ., data=train_data_pca)
+rf <- randomForest(train_data$V1 ~ ., data=train_data_pca, ntree=700, mtry=6)
 rf
+test_pca <- predict(pca_95, test_data[,-1])
+predictions2 <- predict(rf, test_pca)
 
+cm<-table(predictions2, test_data$V1)
+cm
+accuracy(cm)
 
+ # Cross validation
 
-# Cross validation
-
-accs <- c(1:10)
-folds <- createFolds(id$X1, k=10)
-for (i in 1:10) {
+accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
+accs <- c(1:5)
+folds <- createFolds(id$V1, k=5)
+for (i in 1:5) {
   train_split <- id[-folds[[i]],]
   test_split <- id[folds[[i]],]
-  pca_95 <- prcomp(train_data[,-1], rank. = 36)
-  #summary(pca_95)
+  pca_95 <- prcomp(train_split[,-1], rank. = 25)
   train_data_pca <- pca_95$x
+  test_pca <- predict(pca_95, test_split)
   rf <- randomForest(train_split$V1 ~ ., train_data_pca)
-  accs[i] <- rf$err.rate
+  prediction <- predict(rf, test_pca)
+  cm <- table(prediction, test_split$V1)
+  accs[i] <- accuracy(cm)
+  accs[i]
 }
+accs
